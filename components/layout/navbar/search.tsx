5e1eff8347991185d9d10d84ae400375fe77d08e
juliscapucin <jscapucin@gmail.com>
2024-01-30 10:03:20 -0300

    edit header layout

--- components/layout/navbar/search.tsx
+++ components/layout/navbar/search.tsx
@@ -25,7 +25,7 @@ export default function Search() {
   }
 
   return (
-    <form onSubmit={onSubmit} className="w-max-[550px] relative w-full lg:w-80 xl:w-full">
+    <form onSubmit={onSubmit} className="w-max-[550px] relative w-1/3 lg:w-80 xl:w-1/3">
       <input
         key={searchParams?.get('q')}
         type="text"
